c8d11b8d7e81a073bcbe44e7a9909e74c3d0bab1
s25686 <s25686@pjwstk.edu.pl>
2024-04-24 03:27:21 +0200

    WarehouseService.cs Adding service layer to encapsulate business logic for warehouse operations

--- src/Solution7/Solution7/Services/WarehouseService.cs
+++ src/Solution7/Solution7/Services/WarehouseService.cs
@@ -1,6 +1,31 @@
-namespace Solution7.Services;
+using Solution7.Models;
+using System;
 
-public class WarehouseService
+namespace Solution7.Services
 {
-    
+    public class WarehouseService
+    {
+        public WarehouseService()
+        {
+            
+        }
+
+        public bool ValidateProductAndWarehouse(int productId, int warehouseId)
+        {
+            
+            return true; 
+        }
+
+        public bool CheckOrderValidity(int productId, int amount, DateTime createdAt)
+        {
+           
+            return true; 
+        }
+
+        public int UpdateDatabase(ProductWarehouseDto productWarehouse)
+        {
+           
+            return 1; 
+        }
+    }
 }
